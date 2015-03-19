85257dbf109fc913b4b3798246dd923ebb207933
zswang <wjhu111@21cn.com>
2015-03-19 23:44:38 +0800

    测试完成

--- index.js
+++ index.js
@@ -1,43 +1,97 @@
 var fs = require('fs');
 var path = require('path');
+var glob = require('glob');
+var semver = require('semver');
 
-module.exports = function(filename, opts) {
-    opts = opts || {};
-    var root = {};
+// http://registry.npmjs.org/%s/
+// {"dist-tags":{"latest":"0.7.5"},"versions":{"0.0.1":...}}
+// http://registry.npmjs.org/%s/%s
 
-    function parse(filename, node, level) {
-        if (!/\.json$/i.test(filename)) {
-            filename = path.join(filename, 'package.json');
-        }
+module.exports = function(opts) {
+  opts = opts || {};
+  var modules = {};
 
-        if (!fs.existsSync(filename)) {
-            throw new Error('file not exits.');
-        }
+  function loadModule(filename) {
+    try {
+      var json = JSON.parse(fs.readFileSync(filename));
+    }
+    catch (ex) {
+      console.log(ex);
+      return;
+    }
+    modules[json.name] = modules[json.name] || {
+      name: json.name,
+      $$versions: []
+    };
+    if (modules[json.name][json.version]) {
+      return;
+    }
+    modules[json.name].versions = modules[json.name].versions || {};
+    modules[json.name].versions[json.version] = {
+      name: json.name + '@' + json.version,
+      $$package: json
+    };
+    modules[json.name].$$versions.push(json.version);
+  }
 
-        var json = JSON.parse(fs.readFileSync(filename));
-        node.data = {
-            text: json.name + '@' + json.version,
-            note: json.description,
-            hyperlink: json.homepage,
-            hyperlinkTitle: json.homepage ? 'homepage' : undefined,
-            expandState: level < 3 ? 'expand' : undefined
-        };
+  glob('package.json', {
+    sync: true
+  }).forEach(loadModule);
 
-        var children = [];
-        for (var key in json.dependencies) {
-            var item = {};
-            parse(
-                path.join(path.dirname(filename), 'node_modules', key),
-                item, level + 1
-            );
-            children.push(item);
-        }
-        if (children.length) {
-            node.children = children;
-        }
+  var rootModule = modules[Object.keys(modules)[0]];
+  if (!rootModule) {
+    return;
+  }
+
+  glob('node_modules/**/package.json', {
+    sync: true
+  }).forEach(loadModule);
+
+  var root = {};
+
+  function parse(node, name, version, level) {
+    // console.log('parse(name = %j, version = %j)', name, version);
+    var module = modules[name];
+    if (!module) {
+      console.log('error');
+      return;
     }
 
-    parse(filename, root, 0);
+    var versions = module.$$versions.filter(function(v) {
+      return semver.satisfies(v, version, true);
+    }).sort(semver.compareLoose);
 
-    return root;
+    var currVersion = module.versions[versions[0] || module.$$versions[0]];
+    if (!currVersion) {
+      console.log('error');
+      return;
+    }
+
+    node.data = {
+      text: currVersion.$$package.name + '@' + currVersion.$$package.version,
+      note: currVersion.$$package.description,
+      hyperlink: currVersion.$$package.homepage,
+      hyperlinkTitle: currVersion.$$package.homepage ? 'homepage' : undefined,
+      expandState: level < 3 ? 'expand' : 'collapse'
+    };
+
+    var children = [];
+    for (var key in currVersion.$$package.dependencies) {
+      var item = {};
+      parse(item,
+        key,
+        currVersion.$$package.dependencies[key],
+        level + 1
+      );
+      if (Object.keys(item).length) {
+        children.push(item);
+      }
+    }
+    if (children.length) {
+      node.children = children;
+    }
+  }
+  parse(root, rootModule.name, rootModule.$$versions[0], 0);
+
+  return root;
 };
